52b034616b8212bb26c6f800d1090452e6153468
Jeremy <Jeremythegreat95@gmail.com>
2022-05-02 21:29:31 +0800

    first commit

---
+++ js/app.js
@@ -0,0 +1,212 @@
+const container = document.querySelector(".container");
+const albumImage = document.querySelector(".album-image");
+const songName = document.querySelector(".song-name");
+const artistName = document.querySelector(".artist-name");
+const audioSource = document.querySelector("#song");
+const songInfo = document.querySelector(".song-info");
+const btnPlayPause = document.querySelector(".btn-play-pause");
+const btnPrev = document.querySelector(".btn-previous");
+const btnNext = document.querySelector(".btn-next");
+const btnLoop = document.querySelector(".btn-loop");
+const btnShuffle = document.querySelector(".btn-shuffle");
+const btnLike = document.querySelector(".btn-like");
+const progressContainer = document.querySelector(".progress");
+const progressBar = document.querySelector(".progress-bar");
+const modal = document.querySelector(".modal");
+const modalContent = document.querySelector(".modal-content");
+const volumeSlider = document.querySelector(".volume-slider");
+const currentYear = document.querySelector("#year");
+
+let songIndex = 0;
+let hasLiked = false;
+let isLooping = false;
+let isShuffling = false;
+
+window.addEventListener("load", () => {
+  loadSong(songIndex);
+});
+
+function loadSong(songs) {
+  songName.innerText = musicPlayer[songs].songName;
+  artistName.innerText = musicPlayer[songs].artistName;
+  albumImage.src = `assets/images/album/${musicPlayer[songs].img}.jpg`;
+  audioSource.src = `assets/songs/${musicPlayer[songs].src}.mp3`;
+}
+
+function playSong() {
+  container.classList.add("play");
+  btnPlayPause.querySelector("i.fas").classList.remove("fa-play");
+  btnPlayPause.querySelector("i.fas").classList.add("fa-pause");
+  audioSource.play();
+}
+
+function pauseSong() {
+  container.classList.remove("play");
+  btnPlayPause.querySelector("i.fas").classList.remove("fa-pause");
+  btnPlayPause.querySelector("i.fas").classList.add("fa-play");
+  audioSource.pause();
+}
+
+function prevSong() {
+  if (isShuffling === true) {
+    shuffleSong();
+    playSong();
+  } else if (isLooping === true) {
+    repeatSong();
+    playSong();
+  } else {
+    songIndex--;
+    songIndex < 0 ? (songIndex = 0) : (songIndex = songIndex);
+    loadSong(songIndex);
+    playSong();
+  }
+}
+
+function nextSong() {
+  if (isShuffling === true) {
+    shuffleSong();
+    playSong();
+  } else if (isLooping === true) {
+    repeatSong();
+    playSong();
+  } else {
+    songIndex++;
+    songIndex > musicPlayer.length - 1 ? (songIndex = 0) : (songIndex = songIndex);
+    loadSong(songIndex);
+    playSong();
+  }
+}
+
+function shuffleSong() {
+  let randSong = Math.floor(Math.random() * musicPlayer.length + 1);
+  songIndex = randSong;
+  loadSong(songIndex);
+}
+
+function repeatSong() {
+  let currentTrack = songIndex;
+  loadSong(currentTrack);
+}
+
+function updateProgressBar(evt) {
+  const currentTime = evt.target.currentTime;
+  const duration = evt.target.duration;
+  let currentProgress = (currentTime / duration) * 100;
+  progressBar.style.width = `${currentProgress}%`;
+
+  let currentPlaying = document.querySelector(".current-time");
+  let totalDuration = document.querySelector(".total-duration");
+
+  audioSource.addEventListener("loadeddata", () => {
+    // Duration
+    let audioDuration = audioSource.duration;
+    let totalMin = Math.floor(audioDuration / 60);
+    let totalSec = Math.floor(audioDuration % 60);
+
+    if (totalSec < 10) {
+      totalSec = `0${totalSec}`;
+    }
+
+    totalDuration.innerText = `${totalMin}:${totalSec}`;
+  });
+
+  // Current Time
+  let audioPlaying = audioSource.currentTime;
+  let currentMin = Math.floor(audioPlaying / 60);
+  let currentSec = Math.floor(audioPlaying % 60);
+
+  if (currentSec < 10) {
+    currentSec = `0${currentSec}`;
+  }
+
+  currentPlaying.innerText = `${currentMin}:${currentSec}`;
+}
+
+function setProgressBar(evt) {
+  const width = this.clientWidth;
+  const clickX = evt.offsetX;
+  const duration = audioSource.duration;
+
+  audioSource.currentTime = (clickX / width) * duration;
+}
+
+function setVolume() {
+  audioSource.volume = volumeSlider.value / 100;
+}
+
+function likedSong() {
+  btnLike.classList.add("fas");
+  btnLike.classList.remove("far");
+  btnLike.classList.add("active");
+  hasLiked = true;
+  modalPopUp();
+}
+
+function unLikedSong() {
+  btnLike.classList.add("far");
+  btnLike.classList.remove("fas");
+  btnLike.classList.remove("active");
+  hasLiked = false;
+  modalPopUp();
+}
+
+function modalPopUp() {
+  if (hasLiked === true) {
+    modal.style.opacity = 1;
+    modal.style.display = "block";
+    modalContent.innerText = `Added to Liked Songs.`;
+  } else {
+    modal.style.opacity = 1;
+    modal.style.display = "block";
+    modalContent.innerText = `Removed from Liked Songs.`;
+  }
+
+  setTimeout(() => {
+    modal.style.opacity = 0;
+  }, 2000);
+}
+
+btnPlayPause.addEventListener("click", () => {
+  const isPlaying = container.classList.contains("play");
+  isPlaying ? pauseSong() : playSong();
+});
+
+btnPrev.addEventListener("click", prevSong);
+btnNext.addEventListener("click", nextSong);
+
+btnLoop.addEventListener("click", () => {
+  if (!btnLoop.classList.contains("active")) {
+    btnLoop.classList.add("active");
+    isLooping = true;
+    console.log("currently looping");
+  } else {
+    btnLoop.classList.remove("active");
+    isLooping = false;
+    console.log("stop looping");
+  }
+});
+
+btnShuffle.addEventListener("click", () => {
+  if (!btnShuffle.classList.contains("active")) {
+    btnShuffle.classList.add("active");
+    isShuffling = true;
+    console.log("currently shuffling");
+  } else {
+    btnShuffle.classList.remove("active");
+    isShuffling = false;
+    console.log("stop shuffling");
+  }
+});
+
+audioSource.addEventListener("timeupdate", updateProgressBar);
+audioSource.addEventListener("ended", nextSong);
+
+progressContainer.addEventListener("click", setProgressBar);
+
+btnLike.addEventListener("click", () => {
+  const hasLiked = !btnLike.classList.contains("active");
+  hasLiked ? likedSong() : unLikedSong();
+});
+
+//current year
+currentYear.innerText = new Date().getFullYear();
---
+++ js/playlist.js
@@ -0,0 +1,32 @@
+let musicPlayer = [
+  {
+    songName: "A Cruel Angel's Thesis",
+    artistName: "Yoko Takahashi",
+    img: "photo-1",
+    src: "song-1",
+  },
+  {
+    songName: "Butterfly Kiss",
+    artistName: "Chihiro Yonekura",
+    img: "photo-2",
+    src: "song-2",
+  },
+  {
+    songName: "Pray",
+    artistName: "Hunter x Hunter",
+    img: "photo-3",
+    src: "song-3",
+  },
+  {
+    songName: "Again",
+    artistName: "YUI",
+    img: "photo-4",
+    src: "song-4",
+  },
+  {
+    songName: "Romeo and Cinderella",
+    artistName: "Hatsune Miku",
+    img: "photo-5",
+    src: "song-5",
+  },
+];
